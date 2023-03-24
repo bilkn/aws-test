8c4e486d846e6378eac2824237c49d6253acd4b4
bilkn <bilkan.konus@gmail.com>
2023-03-24 20:47:37 +0300

    add html

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 3000;
 
+
 app.listen(PORT,()=>{
     console.log(`App is listening on port ${PORT}.`)
 })
 
-app.get('/api',(req,res)=>{
-    res.send('<h1>Welcome to the EC2 INSTANCE!</h1>')
+app.get('/home',(req,res)=>{
+    res.sendFile('index.html', {root: __dirname })
 })
 
 
